2d032b619695f9a154b43d0ad4005a438ed6b65f
Ashish Bollam <2021eeb1158@iitrpr.ac.in>
2023-07-10 00:48:44 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const server = require('http').createServer(app);
 const PORT = process.env.PORT || 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://chat-app-main-eight.vercel.app',
     methods: ['GET', 'POST']
   }
 });
