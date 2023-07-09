1c5a394445b8f8361a4c7cbf3be3b7c1aab75537
Ashish Bollam <2021eeb1158@iitrpr.ac.in>
2023-07-09 23:45:00 +0530

    added

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ mongoose.connect(`${process.env.MONGO_URL}`,{ useNewUrlParser: true, useUnifiedT
   });
 
 const server = require('http').createServer(app);
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 const io = require('socket.io')(server, {
   cors: {
     origin: 'http://localhost:3000',
